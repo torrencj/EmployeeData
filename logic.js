3331e6cc3fc0c2319706ef7976e616608cc75c44
Lisa Newhouse <lisanewhouse@icloud.com>
2017-08-22 13:16:54 -0500

    moved script to js file

--- logic.js
+++ logic.js
@@ -1,17 +1,27 @@
 
 
-<tr>
-  <td>"name"</td>
-  <td>"role"</td>
-  <td>"start"</td>
-  <td>"sometext"</td>
-</td>
+// Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAKei4rwl2i5hyaPNjMeKT0kqyS2nyOYBE",
+      authDomain: "learning-6f29b.firebaseapp.com",
+      databaseURL: "https://learning-6f29b.firebaseio.com",
+      projectId: "learning-6f29b",
+      storageBucket: "learning-6f29b.appspot.com",
+      messagingSenderId: "161028966778"
+    };
+    firebase.initializeApp(config);
 
-// Function to generate a new table row with passed-in data.
-function addRow(name, role, start, role) {
 
-  var newRow = $("<tr>").append( $("<td>").text("name") )
-                        .append( $("<td>").text("role") )
-                        .append( $("<td>").text("start") )
-                        .append( $("<td>").text("rate") )
-}
+    function addRow(name, role, start, rate) {
+      var monthsworked = 12;
+      var total = "total!";
+
+      var newRow = $("<tr>").append( $("<td>").text(name) )
+                            .append( $("<td>").text(role) )
+                            .append( $("<td>").text(start) )
+                            .append( $("<td>").text(monthsworked) )
+                            .append( $("<td>").text(rate) )
+                            .append( $("<td>").text(total) );
+      $("tbody").append(newRow);
+    };
+
