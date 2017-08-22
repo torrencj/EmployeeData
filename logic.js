fdba18314e534cc85c6efcf490fd7ef5383f8998
John <john@Mac.local>
2017-08-22 14:23:38 -0500

    added id to submit button

--- logic.js
+++ logic.js
@@ -14,13 +14,13 @@
     //creating a variable to reference the database
     var database = firebase.database();
 
-    //initial values from the database
+    // initial values from the database
 
 
 
     //database.ref().on("value", function(snapshot) {
 
-    
+
     // function to create the new row
     function addRow(name, role, start, rate) {
       var monthsworked = 12;
@@ -29,20 +29,30 @@
       var newRow = $("<tr>").append( $("<td>").text(name) )
                             .append( $("<td>").text(role) )
                             .append( $("<td>").text(start) )
-                            .append( $("<td>").text(monthsWorked) )
+                            // .append( $("<td>").text(monthsWorked) )
                             .append( $("<td>").text(rate) )
                             .append( $("<td>").text(total) );
       $("tbody").append(newRow);
 
-      database.ref().set({
-      	name:name,
-      	role : role,
-      	start: start,
-      	rate: rate
-      })
+      // if (true) {
+      //   database.ref().set({
+      //     "name": name,
+      //     "role" : role,
+      //     "start": start,
+      //     "rate": rate
+      //   })
+      // } else {
+        database.ref().push({
+          "name": name,
+          "role" : role,
+          "start": start,
+          "rate": rate
+        })
+      
+
     };
 
-	//});	
+	//});
  // submit button to push the data up to the employees table
 $("#submit-button").on("click", function(event){
 	 event.preventDefault();
@@ -50,7 +60,12 @@ $("#submit-button").on("click", function(event){
 	var name = $("#name").val().trim();
 	var role = $("#role").val().trim();
 	var start = $("#startDate").val().trim();
-	var rate = $("#monthyRate").val();
+	var rate = $("#monthlyRate").val();
+  // console.log(name);
+  // console.log(role);
+  // console.log(start);
+  // console.log(rate);
+
 
 	addRow(name, role, start, rate);
 	console.log("clicked");
