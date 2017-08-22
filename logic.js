2334c58e2c0afc9a72c0ec1c55fb1842343498e2
John <john@Mac.local>
2017-08-22 12:57:00 -0500

    added function to add a new row

--- logic.js
+++ logic.js
@@ -1,7 +1,17 @@
 
 
+<tr>
+  <td>"name"</td>
+  <td>"role"</td>
+  <td>"start"</td>
+  <td>"sometext"</td>
+</td>
+
 // Function to generate a new table row with passed-in data.
-// function addRow(name, role, start, role) {
-//
-//   var newRow = $("<tr>").append($("<tr>").text("name"))
-// }
+function addRow(name, role, start, role) {
+
+  var newRow = $("<tr>").append( $("<td>").text("name") )
+                        .append( $("<td>").text("role") )
+                        .append( $("<td>").text("start") )
+                        .append( $("<td>").text("rate") )
+}
